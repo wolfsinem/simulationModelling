91a9a363d5fe568e45118549bed6f9c8318fd0cf
wolfsinem <sinem.ertem@student.hu.nl>
2019-12-08 13:07:40 +0100

    updated euler

--- zout-oplossing/main_update.py
+++ zout-oplossing/main_update.py
@@ -2,39 +2,32 @@ import matplotlib.pyplot as plt
 import numpy as np
    
 def zout_oplossing(num_steps,uitstroom):
-    # watertank = 1000
     instroom = 6
-    zout_in = 0.1
+    zout_in = 0.1 
 
-    tank_1 = np.zeros(num_steps + 1)
-    zout_1 = np.zeros(num_steps + 1) 
-    conc_1 = np.zeros(num_steps + 1) 
+    t = np.zeros(1000 + 1) 
+    z = np.zeros(1000 + 1) 
+    c = np.zeros(1000 + 1)  
 
-    tank_1[0] = 1000
-    zout_1[0] = 0
-    conc_1[0] = zout_1[0] / tank_1[0]
+    t[0] = 1000
+    z[0] = 0
+    c[0] = z[0] / t[0]
 
-    times = [0]
-    waarden = [0]
-
+    times = [1]
     for step in range(num_steps):
-        tank_1[step+1] = tank_1[step] + instroom - uitstroom
-        zout_1[step+1] = zout_1[step] + (instroom * zout_in)
-        conc_1[step+1] = zout_1[step] / tank_1[step]
-
-        zout_1[step+1] = zout_1[step] - (conc_1[step] * uitstroom)
-        conc_1[step+1] = zout_1[step] / tank_1[step]
-
+        t[step+1] = t[step] + instroom - uitstroom
+        z[step+1] = z[step] + (instroom * zout_in) - (uitstroom * c[step])
+        c[step+1] = z[step] / t[step]
+        
         times.append(step)
-        waarden.append(conc_1)
+    
+    return times, c
 
-    return times,waarden
+times, conc_tank_1 = zout_oplossing(1000,6)
+times, conc_tank_2 = zout_oplossing(1000,5)
 
-time_1, conc_1 = zout_oplossing(1000,5)
-time_2, conc_2 = zout_oplossing(1000,6)
-
-plt.plot(time_1,conc_1,'r',label='speed=5')
-plt.plot(time_2,conc_2,'r',label='speed=6')
+plt.plot(times,conc_tank_1,'r',label='uitstroom=6')
+plt.plot(times,conc_tank_2,'r',label='uitstroom=5')
 plt.title('Zoutoplossing in een tank')
 plt.legend()
 plt.show()
