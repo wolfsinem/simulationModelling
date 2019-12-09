d01a8460bfea65e7f7817f64bc7dd3e3053a60bb
wolfsinem <sinem.ertem@student.hu.nl>
2019-12-09 14:31:33 +0100

    second try

--- hoge-orde/hoge_orde.py
+++ hoge-orde/hoge_orde.py
@@ -2,10 +2,9 @@ import math
 import numpy as np
 import matplotlib.pyplot as plt
 
-e = math.e
-h = 0.01
-
 def euler_method(num_steps):
+    h = 0.01
+    
     x = np.zeros(num_steps+1)
     y = np.zeros(num_steps+1)
 
@@ -13,26 +12,43 @@ def euler_method(num_steps):
     y[0] = 1
 
     for step in range(num_steps):
-        x[step+1] = h * (step + 1)
+        x[step+1] = x[step] + h 
         y[step+1] = y[step] + h * x[step]
-    return x,y
+    return x, y
 
+# for num_steps in [5,10,20,100]:
+#     x,y = euler_method(num_steps)
 
-# def heun_method(num_steps,h):
-#     x = np.zeros(num_steps+1)
-#     y = np.zeros(num_steps+1)
+x,y = euler_method(5)
+x2,y2 = euler_method(10)
+x3,y3 = euler_method(20)
+x4,y4 = euler_method(100)
 
-#     x[0] = 0
-#     y[0] = 1
+def heun_method(num_steps):
+    e = math.e
+    h = 0.01
 
-#     for step in range(num_steps+1):
+    x = np.zeros(num_steps+1)
+    y = np.zeros(num_steps+1)
 
-n = [5,10,20,100]
-for size in n:
-    x,y = euler_method(size)
+    x[0] = 0
+    y[0] = 1
+
+    for step in range(num_steps):
+        xE = x[step] + h * y[step]
+        yE = y[step] + h
+
+        x[step+1] = x[step] + h * 0.5 * (y[step] + yE)
+        y[step+1] = y[step] + h * 0.5
+        
+    return x, y 
+
+a, b = heun_method(20)
 
 def plot_me():
-    plt.plot(x,y)
+    plt.plot(a,b,'r')
+    # plt.plot(x2,y2, 'b')
+    # plt.plot(x3,y3, 'g')
+    # plt.plot(x4,y4, 'y')
     plt.show()
-
 plot_me()
