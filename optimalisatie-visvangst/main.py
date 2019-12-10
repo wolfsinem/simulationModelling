261c848bd4c4a44f29e6f1db6cf6a6392ee08143
wolfsinem <sinem.ertem@student.hu.nl>
2019-12-10 17:17:20 +0100

    cleaned a bit

--- optimalisatie-visvangst/main.py
+++ optimalisatie-visvangst/main.py
@@ -13,19 +13,23 @@ harvest() function shows when no harvesting is taking place
 At the beginning there is expontential growth but after a while
 the amount of fish will approach the maximum carrying capacity
 """
+maximum_growth_rate = 0.5
+carrying_capacity = 2e6 
+maximum_harvest_rate = 0.7 * 2.5e5 # vangst mag maar toenemen tot 0.7 MSY 
+
+end_time = 10.
+h = 0.1
+num_steps = int(end_time / h)
+times = h * np.array(range(num_steps + 1))
+
 def harvest():
-    maximum_growth_rate = 0.5
-    carrying_capacity = 2e6 
-    end_time = 10.
-    h = 0.1
-    num_steps = int(end_time / h)
-    times = h * np.array(range(num_steps + 1))
-    data = []
-    maximum_harvest_rate = 0.7 * 2.5e5
     ramp_start = 2.
     ramp_end = 6. 
+    
     fish = np.zeros(num_steps + 1)
     fish[0] = 2e5
+
+    data = []
 
     is_extinct = False
     for step in range(num_steps):
@@ -44,26 +48,15 @@ def harvest():
             if fish[step+1] <= 0.:
                 is_extinct = True
                 fish[step+1] = 0.
-        fish[step+1] = fish[step+1]
-
     return fish
 
 def total_harvest():
-    maximum_growth_rate = 0.5
-    carrying_capacity = 2e6 
-    maximum_harvest_rate = 0.7 * 2.5e5 #vangst mag maar toenemen tot 0.7 MSY 
-
-    end_time = 10. 
-    h = 0.1 
-    num_steps = int(end_time / h)
-    times = h * np.array(range(num_steps + 1))
-
     fish = np.zeros(num_steps + 1) 
     fish[0] = 2e5
 
     results = []
 
-    for ramp_start in np.arange(2., 10.01, 0.5): #ramp mag pas over 2 jaar starten 
+    for ramp_start in np.arange(2., 10.01, 0.5): # ramp mag pas over 2 jaar starten 
         for ramp_end in np.arange(ramp_start, 10.01, 0.5): 
             total_harvest = 0.
             is_extinct = False
@@ -85,7 +78,6 @@ def total_harvest():
                         is_extinct = True
                         current_harvest = fish[step]
                         fish[step+1] = 0.
-                fish[step + 1] = fish[step+1]
                 total_harvest += current_harvest
             results.append([ramp_start, ramp_end, total_harvest])
 
@@ -112,5 +104,3 @@ def plot_total_harvest():
     axes.set_xlabel('Ramp start in years')
     axes.set_ylabel('Ramp end in years')
 plot_total_harvest()
-
-# %%
