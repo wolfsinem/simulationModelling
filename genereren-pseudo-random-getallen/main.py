265486eb28f446ec60e5c985b47d33a84c918a12
wolfsinem <sinem.ertem@student.hu.nl>
2019-12-12 01:32:17 +0100

    //

--- genereren-pseudo-random-getallen/main.py
+++ genereren-pseudo-random-getallen/main.py
@@ -2,11 +2,39 @@ import numpy as np
 import math
 
 #Linear Congenital Generator (LCG)
-def pseudo_number_generator():
-    x0 = 1
+def pseudo_number_generator(previous):
+    if previous is None:
+        x = 1
+    else:
+        x = previous
+
     a = 2
     c = 3
     m = 5 
+    return (a*x+c)%m
 
-    xn = x0
-    new = ( a * Xn ) + c % m
+def sequence():
+    list = []
+    for i in range(5):
+        n = pseudo_number_generator(i)
+        list.append(n)
+    return list
+
+a = sequence()
+print(a)
+
+"""
+pseude_numer_generator(None)  output: 0 
+pseude_numer_generator(0)     output: 3 
+pseude_numer_generator(3)     output: 4
+pseude_numer_generator(4)     output: 1 
+
+x1 = 2 * 1 + 3 (mod 5)
+   = 0
+x2 = 2 * 0 + 3 (mod 5)
+   = 3 
+x3 = 2 * 3 + 3 (mod 5)
+   = 4 
+x4 = 2 * 4 + 3 (mod 5)
+   = 1 
+"""
