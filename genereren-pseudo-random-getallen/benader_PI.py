711e5cf7e4335f5bf3ce2ac5c34341eec932196f
wolfsinem <sinem.ertem@student.hu.nl>
2019-12-18 20:30:29 +0100

    pi plotten

--- genereren-pseudo-random-getallen/benader_PI.py
+++ genereren-pseudo-random-getallen/benader_PI.py
@@ -1,21 +1,55 @@
 from main import pseudo_number_generator, plot_dist
+import math
+import numpy as np
+import matplotlib.pyplot as plt
 
 """
 Pi benaderen met de 'Random Number Generator'
 'Monte Carlo integration'
+
+https://measureofdoubt.com/2018/07/22/finding-pi-from-random-numbers/
+
 """
 def pi_benaderen():
-    random_nums = pseudo_number_generator(num_steps=1000000,m=1)
-    x = random_nums[:len(random_nums)//2]
-    y = random_nums[len(random_nums)//2:]
+    pseudo_nums = pseudo_number_generator(num_steps=1000,m=1)
+    x = pseudo_nums[:len(pseudo_nums)//2]
+    y = pseudo_nums[len(pseudo_nums)//2:]
 
-    inside = 0
-    outside = 0
+    inside = [[],[]]
+    outside = [[],[]]
+    total_inside = 0
+    total_outside = 0
     for (i,j) in zip(x,y):
-        if math.sqrt(i**2+j**2) <=1:
-            inside+=1
+        if math.sqrt(i**2+j**2)**0.5 <=1:
+            inside[0].append(i)
+            inside[1].append(j)
+            total_inside+=1
         else:
-            outside+=1
+            outside[0].append(i)
+            outside[1].append(j)
+            total_outside+=1
 
-    pi = (4.*inside)/len(zip(x,y))
-    return pi
+    pi = (4.*total_inside)/len(zip(x,y))
+    print('Pi benadering bij random gegenereerde getallen: {}'.format(pi))
+    return inside,outside
+
+def plot_cirkle():
+    x,y = pi_benaderen()
+    theta = np.linspace(0, np.pi/2, 100)
+    r = np.sqrt(1.0)
+
+    x1 = r*np.cos(theta)
+    x2 = r*np.sin(theta)
+
+    fig, ax = plt.subplots(1)
+
+    ax.plot(x1, x2)
+    ax.set_aspect(1)
+
+    plt.scatter(x[0],x[1])
+    plt.scatter(y[0],y[1])
+
+    plt.grid(linestyle='--')
+    plt.show()
+
+plot_cirkle() 
